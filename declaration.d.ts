f6c1e31bb3f1f1a53161996c3b773e98d2d34f4f
aryabov <erazorrr@gmail.com>
2023-11-14 21:36:45 +0300

    fixed tsc css & svg

---
+++ declaration.d.ts
@@ -0,0 +1,15 @@
+// declaration.d.ts
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module '*.css' {
+  const content: Record<string, string>;
+  export default content;
+}
+
+declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
