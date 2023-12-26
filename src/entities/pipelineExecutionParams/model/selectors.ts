1b98459210b5770fe06f00c129d7e180fa4d017e
Kirill Suvorov <kirill@fidi.tech>
2023-12-26 16:06:27 +0300

    fixed rjsf keyword problem

--- src/entities/pipelineExecutionParams/model/selectors.ts
+++ src/entities/pipelineExecutionParams/model/selectors.ts
@@ -1,6 +1,23 @@
+import {PipelineId} from '@/entities/pipeline/model';
+
 import {pipelineExecutionParamsAdapter} from './adapter';
 
 export const {selectById} = pipelineExecutionParamsAdapter.getSelectors(
   // @ts-expect-error
   state => state.pipelineExecutionParams
 );
+
+/*
+ * Keyword "id" cannot be used in rjsf, using "$id" for schema ID
+ */
+export const selectSafeRJSFPipelineExecutionParams = (state: any, pipelineId: PipelineId) => {
+  const original = selectById(state, pipelineId);
+  if (!original) {
+    return original;
+  }
+  const {id, ...rest} = original;
+  return {
+    '$id': id,
+    ...rest,
+  };
+}
--- src/entities/pipelineExecutionParams/model/types.ts
+++ src/entities/pipelineExecutionParams/model/types.ts
@@ -1,10 +1,13 @@
 import {PipelineId} from '@/entities/pipeline/model';
 
-export type PipelineExecutionParams = {
-  is: PipelineId,
+export type PipelineExecutionParamsRaw = {
   description: string,
   properties: any,
   required: string[],
   title: string,
   type: string,
+}
+
+export type PipelineExecutionParams = PipelineExecutionParamsRaw & {
+  id: PipelineId,
 }
