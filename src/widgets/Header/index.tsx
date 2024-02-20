98e75f33b93468330c0f72fb8cdcbe2aa70d9799
Anton <anton@fidi.tech>
2024-02-20 21:22:47 +0300

    small fixes for the deployment

--- src/widgets/Header/index.tsx
+++ src/widgets/Header/index.tsx
@@ -13,11 +13,11 @@ const Header = () => {
   const disconnect = () => window.location.assign('/api/auth/logout');
   return (
     <Module className={styles.root}>
-      <a href="https://dot.fidi.tech" className={styles.badge}>
+      <a href={window.location.pathname.startsWith("/v2") ? "https://dot.fidi.tech/v2" : 'https://dot.fidi.tech/'} className={styles.badge}>
         <div className={styles.logo} />
       </a>
       <div className={styles.links}>
-        <Link href="/" className={styles.link}>Code-Free Analytics</Link>
+        <Link href={window.location.pathname.startsWith("/v2") ? "/v2" : '/'} className={styles.link}>Code-Free Analytics</Link>
       </div>
       <Button onClick={disconnect} icon={<Icons.Disconnect />} theme="muted" className={styles.disconnect} />
     </Module>
