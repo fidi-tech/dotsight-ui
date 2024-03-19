5ac11f5060c58f4c64ba5d7613f598c22fe1e033
Anton <anton@fidi.tech>
2024-03-19 22:35:44 +0300

    fixed unit tests

--- src/shared/lib/date.spec.ts
+++ src/shared/lib/date.spec.ts
@@ -5,7 +5,7 @@ describe('shared/lib/date', () => {
     it('works correct', () => {
       expect(timestampToStartOfTheDay(12345678)).toEqual(12268800);
       expect(timestampToStartOfTheDay(123456789)).toEqual(123379200);
-      expect(timestampToStartOfTheDay(1234567890)).toEqual(1234558800);
+      expect(timestampToStartOfTheDay(1234567890)).toEqual(1234483200);
     })
   });
 })
